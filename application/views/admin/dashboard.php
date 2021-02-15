536ead848dc450e2e7f31ee8f59ab25b522ce932
Andikariski <andikariski207@gmail.com>
2021-02-15 10:08:44 +0700

    final

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -64,50 +64,12 @@
             <!-- ./col -->
         </div>
 
-        <!-- BAR CHART -->
-        <div class="row mt-2 p-3">
-            <div class="col">
-                <div class="card card-success">
-                    <div class="card-header">
-                        <h3 class="card-title">Bar Chart</h3>
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart">
-                            <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-            </div>
-            <div class="col">
-                <!-- DONUT CHART -->
-                <div class="card card-danger">
-                    <div class="card-header">
-                        <h3 class="card-title">Donut Chart</h3>
 
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-            </div>
-        </div>
+</div>
 
-        <!-- /.card -->
-    </section>
-    <!-- /.card -->
+<!-- /.card -->
+</section>
+<!-- /.card -->
 </div>
 </div>
 </div>
--- application/views/tamplateAdmin/header.php
+++ application/views/tamplateAdmin/header.php
@@ -26,9 +26,10 @@
     <link rel="stylesheet" href="<?= base_url('assets/') ?>AdminLTE/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
     <link rel="stylesheet" href="<?= base_url('assets/') ?>AdminLTE/plugins/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>AdminLTE/dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>AdminLTE/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- DataTables -->
